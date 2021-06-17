5ff98494fd471c9353a6bf1d2d3c4197796abd16
Sidak1311 <67144572+Sidak1311@users.noreply.github.com>
2021-06-17 21:22:30 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Home from './screens/screen1'
+import Screnn from './screens/screen2'
+import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native'
+import 'react-native-gesture-handler'
+
+const Stack = createStackNavigator()
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName = 'Home' screenOptions ={{headerShown:false}}>
+        <Stack.Screen name = 'Home' component = {Home}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
