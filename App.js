52e8e7422ba9900244734dfab45e359fc71acd0c
Sidak1311 <67144572+Sidak1311@users.noreply.github.com>
2021-06-17 21:23:11 +0530

    Update App.js

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native'
 import 'react-native-gesture-handler'
 
+//Will add the images later coz i need to go to sleep :P
+
 const Stack = createStackNavigator()
 
 export default function App() {
